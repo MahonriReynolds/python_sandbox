862a74bc903b96c655a40792c553fb764c8f82ee
Mahonri Reynolds <107807737+MahonriReynolds@users.noreply.github.com>
2024-06-07 18:48:26 -0600

    Update main.py

--- Boids/main.py
+++ Boids/main.py
@@ -4,7 +4,7 @@ from Field import Field
 
 def main() -> None:
     # make Field object with flocks of sizes 15, 10, and 12
-    field = Field([30, 15, 25])
+    field = Field([15, 10, 12])
     # run the Field's display in a loop
     while True:
         field.view()
