a705dd0f301470673a4b42fd89200e0054ec2567
MahonriReynolds <mahonri.r66@gmail.com>
2024-06-07 18:42:20 -0600

    Added misc scripts with first example of trace table gen

---
+++ Misc/trace_table.py
@@ -0,0 +1,64 @@
+
+# Add this snippet to the top of small scripts to genrate 
+# a variable trace table in same foler as 'trace.txt'.
+# Assumes code to be traced is in a function and will be 
+# handling variables as locals.
+
+
+# Implimentation of sys.settrace().
+import sys
+
+# Set up trace function to handle formatting of trace table.
+def trace(frame, *_):
+    
+    # Move the variable names out two tabs which will accomodate up 
+    # to 999 lines without messing up the formatting.
+    header = '\t\t'
+    
+    # Start off the next trace line with the line number.
+    next_line = f'\n{frame.f_lineno}\t\t'
+
+    # For each local value, turn the name and value into a 
+    # string and find the difference in their lengths. 
+    # The difference is used to fill the empy distance between 
+    # the two so the columns stay aligned in the table.
+    for v in frame.f_locals:
+        h = str(v)
+        n = str(frame.f_locals[v])
+        filler = ' ' * (abs(len(h) - len(n)))
+        
+        # Check if the header or variable value should receive 
+        # the spacer.
+        if len(h) > len(n):
+            n += filler
+        else:
+            h += filler
+        
+        # Space each column out by 2 tabs.
+        header += f'{h}\t\t'
+        next_line += f'{n}\t\t'
+
+    # Use append mode first to gen the file if not exists.
+    # Add the next line of value traces.
+    with open('trace.txt', 'a') as f:
+        f.write(next_line)
+    
+    # Mode r+ wasn't working so I split it up into r and w modes.
+    # file.seek(0, 0) was also making issues.
+    # Read everything out of the trace table file and overwrite 
+    # the first line (prev header) with the new header hen write 
+    # it all back in.
+    with open('trace.txt', 'r') as f:
+        content = f.readlines()
+    content[0] = f'{header}\n'
+    with open('trace.txt', 'w') as f:
+        f.writelines(content)
+
+    # Return trace for sys to use. 
+    return trace
+
+# Use sys to set the trace function as the program trace.
+sys.settrace(trace)
+
+
+# ** main code goes here **
